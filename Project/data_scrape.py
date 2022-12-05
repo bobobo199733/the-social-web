8a0d027115451cabdaf741cbdc0e5dd48904b388
H.zwaan <hugo.zwaan.zwaan@hva.nl>
2022-12-05 14:46:30 +0100

    dataset with 100000 tweets

---
+++ Project/data_scrape.py
@@ -0,0 +1,25 @@
+import json
+
+import tweepy
+
+TWITTER_API_KEY = "8r4bvIuCRsNzrbdPsCqaUoVv9"
+TWITTER_API_SECRET = "JptZjnN991736aSssz4F8RUaOPtTHEOejNyDGb064KAjPJ2GMU"
+TWITTER_ACCESS_TOKEN = "1297797286148399104-JTeBfDEIMD5xDmuzgtmiOcJMrurhFx"
+TWITTER_ACCESS_SECRET = "FmJFEJjFiJEMMRAHenXe1dCNpPD6lz02mKuZVMzBP9b7c"
+
+# Authentificate with Twitter
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+
+total_tweets = 100000
+
+# Create the API object
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+hashtag = "#RIPtwitter -filter:retweets"
+results = tweepy.Cursor(
+    api.search_tweets, q=hashtag, lang="en", tweet_mode="extended"
+).items(total_tweets)
+
+with open("Project/data.json", "w") as f:
+    json.dump([tweet.full_text for tweet in results], f)
